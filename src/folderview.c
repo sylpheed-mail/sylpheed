47db776f78de0ba503f7dbb3a612d0d4ce4fdee5
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2005-06-21 09:37:56 +0000

    modified the alert message on deleting folders.

--- src/folderview.c
+++ src/folderview.c
@@ -2029,7 +2029,8 @@ static void folderview_delete_folder_cb(FolderView *folderview, guint action,
 	name = trim_string(item->name, 32);
 	AUTORELEASE_STR(name, {g_free(name); return;});
 	message = g_strdup_printf
-		(_("All folder(s) and message(s) under `%s' will be deleted.\n"
+		(_("All folders and messages under `%s' will be permanently deleted.\n"
+		   "Recovery will not be possible.\n\n"
 		   "Do you really want to delete?"), name);
 	avalue = alertpanel(_("Delete folder"), message,
 			    _("Yes"), _("+No"), NULL);
