c5db65058ff2edad80c74413a17d0c5f76577a7b
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2005-10-11 11:08:59 +0000

    fixed the label 'Type' not aligned to right at folder property dialog.

--- src/prefs_folder_item.c
+++ src/prefs_folder_item.c
@@ -218,6 +218,7 @@ static void prefs_folder_item_general_create(PrefsFolderItemDialog *dialog)
 	label = gtk_label_new(_("Type"));
 	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 3, 4,
 			 GTK_FILL, 0, 0, 0);
+	gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
 
 	hbox = gtk_hbox_new(FALSE, 8);
 	gtk_widget_show(hbox);
