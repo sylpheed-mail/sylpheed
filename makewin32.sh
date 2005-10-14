e95c1cdd58655140243b0a2a7dbfeec9f5c98a62
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2005-10-14 09:20:49 +0000

    makewin32.sh: modified the optimization flags.

--- makewin32.sh
+++ makewin32.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./configure --with-localedir=share/locale \
+./configure --with-localedir=share/locale 'CC=gcc -mcpu=pentium3' CFLAGS=-O3 \
   && make \
   && make install-strip prefix=$HOME/dist
