688a07b009fa271777ae448b977d89028174aaa0
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2005-11-22 07:46:36 +0000

    modified makewin32.sh.

--- makewin32.sh
+++ makewin32.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./configure --with-localedir=share/locale 'CC=gcc -mcpu=pentium3' CFLAGS=-O3 \
+./configure --with-localedir=share/locale 'CC=gcc -mtune=pentium3' CFLAGS=-O3 \
   && make \
   && make install-strip prefix=$HOME/dist
