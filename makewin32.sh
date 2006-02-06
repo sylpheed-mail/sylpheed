5e6f68a2d0fa6719e6d4f02e450a146ba3559a53
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2006-02-06 07:41:34 +0000

    updated makewin32.sh, and hu.po. Made 2.2.0beta8 release.

--- makewin32.sh
+++ makewin32.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-./configure --with-localedir=share/locale 'CC=gcc -mtune=pentium3' CFLAGS=-O3 \
+./configure --with-localedir=share/locale --disable-ipv6 \
+  'CC=gcc -mtune=pentium3' CFLAGS=-O3 \
   && make \
   && make install-strip prefix=$HOME/dist
