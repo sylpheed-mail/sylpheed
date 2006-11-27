a774197f660a0281586a255a6e8f91629a33b7e0
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2006-11-27 06:15:10 +0000

    fixed the prototype of touufrombits().

--- libsylph/uuencode.h
+++ libsylph/uuencode.h
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-void touufrombits(unsigned char *, const unsigned char *, int);
+int touufrombits(unsigned char *, const unsigned char *, int);
 int fromuutobits(char *, const char *);
 
 #define X_UUENCODE_END_LINE '`'
