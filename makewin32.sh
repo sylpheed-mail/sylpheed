2619b1637d6911ee74d689d9ae5856cb39952a94
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2009-10-08 07:09:43 +0000

    makewin32.sh: added --enable-threads.

--- makewin32.sh
+++ makewin32.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ./configure --prefix=$HOME/dist --with-localedir=lib/locale \
-  --enable-oniguruma --disable-ipv6 \
+  --enable-oniguruma --enable-threads --disable-ipv6 \
   'CC=gcc -mtune=pentium3' CFLAGS=-O3 \
   && make \
   && make install-strip
