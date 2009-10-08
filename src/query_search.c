ad333459d473312d1224a206963c1db8fad88bdf
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2009-10-08 08:24:57 +0000

    destroy async queue when finished.

--- src/query_search.c
+++ src/query_search.c
@@ -598,6 +598,8 @@ static gpointer query_search_folder_func(gpointer data)
 
 	debug_print("query_search_folder_func start\n");
 
+	g_async_queue_ref(qdata->queue);
+
 	mlist = folder_item_get_msg_list(qdata->item, TRUE);
 	qdata->total = g_slist_length(mlist);
 
@@ -657,6 +659,7 @@ static gpointer query_search_folder_func(gpointer data)
 	}
 
 	procmsg_msg_list_free(mlist);
+	g_async_queue_unref(qdata->queue);
 
 	qdata->flag = 1;
 	g_main_context_wakeup(NULL);
@@ -719,7 +722,7 @@ static void query_search_folder(FolderItem *item)
 	g_thread_join(thread);
 	debug_print("query_search_folder: thread exited\n");
 
-	g_async_queue_free(data.queue);
+	g_async_queue_unref(data.queue);
 #else /* !USE_THREADS */
 	query_search_folder_func(&data);
 #endif
