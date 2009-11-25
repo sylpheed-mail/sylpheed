6f303844b4649485fc2d1b1053be179de75ab85b
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2009-11-25 06:47:41 +0000

    updated makewin32.sh.

--- makewin32.sh
+++ makewin32.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/include
+
 ./configure --prefix=$HOME/dist --with-localedir=lib/locale \
   --enable-oniguruma --enable-threads --disable-ipv6 \
   'CC=gcc -mtune=pentium3' CFLAGS=-O3 \
