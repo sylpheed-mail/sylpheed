1618302cc1fec94f04bca9483bdd593ecd8f0859
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2012-06-25 05:18:53 +0000

    possible fix for gmodule link error on some environments (#74). Updated copyright year.

--- libsylph/session.c
+++ libsylph/session.c
@@ -1,6 +1,6 @@
 /*
  * LibSylph -- E-Mail client library
- * Copyright (C) 1999-2010 Hiroyuki Yamamoto
+ * Copyright (C) 1999-2012 Hiroyuki Yamamoto
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
