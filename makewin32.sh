6872471f961d7395957804cae1da152c75a9799a
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2013-09-20 06:39:50 +0000

    makewin32.sh: added --with-themedir configure option.

--- makewin32.sh
+++ makewin32.sh
@@ -3,7 +3,9 @@
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/include
 
-./configure --prefix=$HOME/dist --with-localedir=lib/locale \
+./configure --prefix=$HOME/dist \
+  --with-localedir=lib/locale \
+  --with-themedir=share/icons \
   --enable-oniguruma --enable-threads \
   'CC=gcc -mtune=pentium3' CFLAGS=-O3 \
   && make \
