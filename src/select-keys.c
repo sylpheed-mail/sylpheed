0c63efc9278c9dffc088b96218aac087f998a092
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2014-03-19 02:04:29 +0000

    src/select-keys.c: modified title.

--- src/select-keys.c
+++ src/select-keys.c
@@ -281,7 +281,7 @@ create_dialog (struct select_keys_s *sk)
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_widget_set_size_request (window, 520, 280);
     gtk_container_set_border_width (GTK_CONTAINER (window), 8);
-    gtk_window_set_title (GTK_WINDOW (window), _("Select Keys"));
+    gtk_window_set_title (GTK_WINDOW (window), _("Select encryption keys"));
     gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
     gtk_window_set_modal (GTK_WINDOW (window), TRUE);
     g_signal_connect (G_OBJECT (window), "delete_event",
