119782392b28211638bacae96c14f16400e8287c
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2014-03-26 06:12:56 +0000

    updated copyright year. configure.in: increased library version.

--- src/addr_compl.c
+++ src/addr_compl.c
@@ -2,7 +2,7 @@
  * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
  *
  * Copyright (C) 2000-2005 by Alfons Hoogervorst & The Sylpheed Claws Team.
- * Copyright (C) 1999-2012 Hiroyuki Yamamoto
+ * Copyright (C) 1999-2014 Hiroyuki Yamamoto
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/rfc2015.c
+++ src/rfc2015.c
@@ -1,6 +1,7 @@
 /*
  * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
  * Copyright (C) 2001 Werner Koch (dd9jn)
+ * Copyright (C) 1999-2014 Hiroyuki Yamamoto
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
