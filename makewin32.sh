0f5c2a713f5f4b7ff76ff26ea5745e102c5699f2
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2014-04-10 10:09:05 +0000

    modified for newer MinGW environment. Fixed linking.

--- makewin32.sh
+++ makewin32.sh
@@ -4,10 +4,10 @@ export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/include
 
 ./configure --prefix=$HOME/dist \
-  --with-localedir=lib/locale \
+  --with-localedir=share/locale \
   --with-themedir=share/icons \
   --enable-oniguruma --enable-threads \
-  'CC=gcc -mtune=pentium3' CFLAGS=-O3 \
+  'CC=gcc -mtune=core2' CFLAGS=-O3 \
   && make \
   && make install-strip \
   && (cd plugin/attachment_tool; make install-plugin) \
