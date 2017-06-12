7d562897842eb1ab157cb27fdc96d567dd2d8188
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2017-06-12 04:38:22 +0000

    updated copyright year.

--- src/printing.c
+++ src/printing.c
@@ -1,6 +1,6 @@
 /*
  * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2013 Hiroyuki Yamamoto
+ * Copyright (C) 1999-2017 Hiroyuki Yamamoto
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
